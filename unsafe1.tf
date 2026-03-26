8db3cae043b4f1d7c85da81cf4064c6a166990a3
Keerthana S <keerthanasivan04@gmail.com>
2026-03-26 09:42:49 +0530

    Remove wide open security group from configuration
    Removed a security group that allowed all ports open to the internet.

--- unsafe1.tf
+++ unsafe1.tf
@@ -14,35 +14,6 @@ resource "aws_s3_bucket" "public_data" {
   }
 }
 
-# ❌ Security group with all ports open to the internet
-resource "aws_security_group" "wide_open_sg" {
-  name        = "wide-open-sg"
-  description = "Dangerously open security group"
-  vpc_id      = aws_vpc.splunk-vpc.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "All TCP ports open to the world"
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH open to the world"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 
 
